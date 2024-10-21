dd031a8f5987f92bcdae52a555aa56ec7d8f9b30
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-10-21 15:47:32 +0300

    install nodemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
 import './App.css';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './Components/Pages/Main.jsx';
+import Footer from './Components/Pages/Footer.jsx';
+import Header from './Components/Pages/Header.jsx';
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"></Route>
+        </Routes>
+      </BrowserRouter>
       <h1>Note Taking App</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
