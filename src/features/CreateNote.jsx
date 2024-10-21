990323779bef20939da44143b863b48eaf30f5c3
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-10-22 00:57:21 +0300

    update createNote component adding value

--- src/features/CreateNote.jsx
+++ src/features/CreateNote.jsx
@@ -1,17 +1,24 @@
 import PropTypes from 'prop-types';
 import '../App.css';
 
-const CreateNote = ({ setInputText, setInputContent, saveHandler }) => {
+const CreateNote = ({
+  title,
+  content,
+  setInputText,
+  setInputContent,
+  saveHandler,
+}) => {
   return (
     <>
       <div className="create-note">
         <h2>Create note</h2>
-        <form action="" onClick={saveHandler}>
+        <form action="">
           <input
             type="text"
             id="title"
             name="title"
             placeholder="Title"
+            value={title}
             onChange={(e) => {
               e.preventDefault();
               //   console.log(e.target.value);
@@ -22,6 +29,7 @@ const CreateNote = ({ setInputText, setInputContent, saveHandler }) => {
             name="content"
             id="content"
             placeholder="Content..."
+            value={content}
             onChange={(e) => {
               e.preventDefault();
               //   console.log(e.target.value);
@@ -29,7 +37,9 @@ const CreateNote = ({ setInputText, setInputContent, saveHandler }) => {
             }}
             rows={10}
           ></textarea>
-          <button className="btn_save">Save</button>
+          <button className="btn_save" onClick={saveHandler}>
+            Save
+          </button>
         </form>
       </div>
     </>
@@ -39,6 +49,8 @@ const CreateNote = ({ setInputText, setInputContent, saveHandler }) => {
 export default CreateNote;
 
 CreateNote.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
   setInputText: PropTypes.func.isRequired,
   setInputContent: PropTypes.func.isRequired,
   saveHandler: PropTypes.func.isRequired,
